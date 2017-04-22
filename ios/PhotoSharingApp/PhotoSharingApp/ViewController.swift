9f486dba85dcaa4ae8a8ca4843cb738a4a2e8678
Gustave Rousselet <gustave.rousselet@icloud.com>
2017-04-22 13:56:37 +0200

    Initial commit of iOS app branch.

---
+++ ios/PhotoSharingApp/PhotoSharingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoSharingApp
+//
+//  Created by Gustave Rousselet on 2017-03-24.
+//  Copyright © 2017 Gustave Rousselet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
