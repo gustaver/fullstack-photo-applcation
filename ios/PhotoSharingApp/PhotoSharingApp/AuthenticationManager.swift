bad8b706e5b5ffdc811683891653370f9b4bb579
Gustave Rousselet <gustave.rousselet@icloud.com>
2017-04-23 21:23:00 +0200

    Implement rudimentary login view.

--- ios/PhotoSharingApp/PhotoSharingApp/AuthenticationManager.swift
+++ ios/PhotoSharingApp/PhotoSharingApp/AuthenticationManager.swift
@@ -14,38 +14,47 @@ class AuthenticationManager {
 
     // API call settings
     var baseUrl: String
+    var ip: String
+    var port: String
 
     // Token for future API calls after login
     var Token: String
 
     init() {
-        self.baseUrl = "http://192.168.1.3:8080"
+        // Assumed initial state, user needs to provide port and IP
+        self.baseUrl = "http://"
         self.Token = ""
+        self.ip = ""
+        self.port = ""
     }
 
-    func loginUser(username: String, password: String, complete: @escaping (_ response: String) -> Void) {
+    func loginUser(username: String, password: String, completeCallback: @escaping (_ title: String, _ message: String, _ succesful: Bool) -> Void) {
         // Create JSON body of username and password
         let parameters: Parameters = ["username": username, "password": password]
 
-        // Create url
-        let url = baseUrl + "/login"
+        // Create url from parameters set in fields (by user from LoginView text fields)
+        let url = baseUrl + ip + ":" + port + "/login"
         // Make request
         Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseJSON { response in
+            if response.response === nil {
+                // Invalid url 
+                completeCallback("Login failed", "Invalid IP or port, try again", false)
+            }
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 self.Token = json["token"].stringValue
                 // Use callback closure to send back response
-                complete("Login Succesful")
+                completeCallback("Login Succesful", "Welcome!", true)
             case .failure:
                 let statusCode = response.response?.statusCode
                 if statusCode == 401 {
                     // Unathorized 
-                    complete("Invalid login credentials")
+                    completeCallback("Login failed", "Invalid login credentials, please try again", false)
                 }
                 else {
                     // Bad request 
-                    complete("Invalid login credentials")
+                    completeCallback("Login failed", "Invalid login credentials, please try again", false)
                 }
             }
         }
--- ios/PhotoSharingApp/PhotoSharingApp/LoginViewController.swift
+++ ios/PhotoSharingApp/PhotoSharingApp/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  PhotoSharingApp
 //
 //  Created by Gustave Rousselet on 2017-03-24.
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet var usernameTextInput: UITextField!
     @IBOutlet var passwordTextInput: UITextField!
+    @IBOutlet var portTextInput: UITextField!
+    @IBOutlet var ipTextInput: UITextField!
     
     
     override func viewDidLoad() {
@@ -27,13 +29,23 @@ class ViewController: UIViewController {
     @IBAction func onPressLogin(_ sender: Any) {
         // Check that there is input in both username field and password field
         if usernameTextInput.hasText && passwordTextInput.hasText {
-            AuthenticationManager.sharedInstance.loginUser(username: usernameTextInput.text!, password: passwordTextInput.text!) { response in
-                // Callback from response
-                // Display message from callback 
-                self.displayAlert(title: "Login", alertText: response, buttonText: "Ok")
+            // Set the port and IP of AuthenticationManager (if bad, request will fail anyway) 
+            AuthenticationManager.sharedInstance.port = portTextInput.text!
+            AuthenticationManager.sharedInstance.ip = ipTextInput.text!
+            // Callback function to be sent into authentication request
+            func callbackRequestComplete(title: String, message: String, succesful: Bool) {
+                // Display alerty showing message from request
+                self.displayAlert(title: title, alertText: message, buttonText: "Ok")
+                // Check if login was succesful 
+                if succesful {
+                    self.onLoginSuccesful()
+                }
             }
+            // Make authentication request, if succesful, callback will display accordingly
+            AuthenticationManager.sharedInstance.loginUser(username: usernameTextInput.text!, password: passwordTextInput.text!, completeCallback: callbackRequestComplete)
         }
         else {
+            // One of the fields didn't have any text, display alert
             self.displayAlert(title: "Invalid login", alertText: "All login parameters must be specified", buttonText: "Ok")
         }
     }
@@ -42,6 +54,10 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: title, message: alertText, preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: buttonText, style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func onLoginSuccesful() {
+        
     }
 }
 
